df7a4eb84b1bc0b50d2f954d7b03fb8e10312fc8
Bradley Parris <parrisbjp@gmail.com>
2022-05-29 00:55:50 -0400

    display questions and options function

---
+++ assets/js/quiz.js
@@ -0,0 +1,65 @@
+var questionsArray = [
+    {
+        title: "Commonly used data types DO Not Include:",
+        choices: ["strings","booleans","alerts","numbers"],
+        answer: "alerts"
+    },
+    {
+        title: "The condition in an if / else statement is enclosed with ______.",
+        choices: ["quotes","curly brackets","parenthesis","square brackets"],
+        answer: "parenthesis"
+    },
+    {
+        title: "Arrays in JavaScript can be used to store _______.",
+        choices: ["numbers and strings","other arrays","booleans","all of the above"],
+        answer: "all of the above"
+    },
+    {
+        title: "String values must be enclosed within ______ when being assigned to variables",
+        choices: ["commas","curly brackets","quotes","parenthesis"],
+        answer: "quotes"
+    },
+    {
+        title: "A very useful tool used during development and debugginf for printing content to the debugger is:",
+        choices: ["JavaScript","terminal/bash","for loops","console.log"],
+        answer: "console.log"
+    }
+];
+
+var questionsSectionEl = document.querySelector("#questionsSection");
+var wrapperEl = document.querySelector("#wrapper");
+var startButtonEl = document.querySelector("#startBtn");
+var timer = 0;
+
+var listEl = document.createElement("ul");
+
+// Display questions function
+function displayQuestions(questionIndex) {
+    // Removes pre-existing data from page
+    questionsSectionEl.innerHTML = "";
+    
+    // Display question
+    questionsSectionEl.textContent = questionsArray[questionIndex].title;
+
+    // Displays list of choices
+    questionsSectionEl.appendChild(listEl);
+    
+    // Loop for listing off each choice
+    for (var i = 0; i < 4; i++) {
+        // Create list item element
+        var listItemEl = document.createElement("li");
+        // Selects choice, sets text content of list item element
+        listItemEl.textContent = questionsArray[questionIndex].choices[i];
+        // Adds list item to list of questions
+        listEl.appendChild(listItemEl);
+        // Make list items clickable and see if the item clicked is the right answer
+        listItemEl.addEventListener("click", correctAnswer());
+    }
+}
+
+startButtonEl.addEventListener("click", function() {
+    // START TIMER
+
+    // Displays first question on page
+    displayQuestions(0);
+})
