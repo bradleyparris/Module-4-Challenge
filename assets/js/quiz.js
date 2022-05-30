85ad07bddbb85fc8175931976ca2e9d2474d235b
Bradley Parris <parrisbjp@gmail.com>
2022-05-29 20:13:08 -0400

    css style changes

--- assets/js/quiz.js
+++ assets/js/quiz.js
@@ -42,52 +42,61 @@ function clearContent() {
 // Timer function
 function startTimer() {
     setInterval(function() {
-        timer = timer - 1;
+        timer--;
         // Displays time for user
         timeEl.textContent = "Time: " + timer;
-        
+
         // Checks if user has run out of time
         if (timer <= 0) {
-            endGame()
-            // Keeps time from counting down to the negatives
-            timer = 1;
+            endGame();
         }
     }, 1000);
 }
 
 // Display questions function
 function displayQuestions(questionIndex) {
-    clearContent();
+    // If statement for when user runs out of questions
+    if (questionIndex <= 4) {
 
-    // Display question
-    questionsSectionEl.textContent = questionsArray[questionIndex].title;
+        clearContent();
 
-    // Displays list of choices
-    questionsSectionEl.appendChild(listEl);
-    
-    // Loop for listing off each choice
-    for (var i = 0; i < 4; i++) {
-        // Create list item element
-        var listItemEl = document.createElement("li");
-        // Selects choice, sets text content of list item element
-        listItemEl.textContent = questionsArray[questionIndex].choices[i];
-        // Adds list item to list of questions
-        listEl.appendChild(listItemEl);
-        // Make list items clickable and see if the item clicked is the right answer
-        listItemEl.addEventListener("click", function(event) {
-            var option = event.target;
+        // Display question
+        var questionTitle = document.createElement("h2");
+        questionTitle.textContent = questionsArray[questionIndex].title;
+        questionTitle.setAttribute("id","questionTitle");
+        questionsSectionEl.appendChild(questionTitle);
 
-            if (option.textContent === questionsArray[questionIndex].answer) {
-                // Runs the function all over again, except this time displays the next question
-                displayQuestions(questionIndex + 1);
-            }
-            else {
-                // Subtract time from the clock
-                timer = timer - 15;
-                // Runs the function all over again, except this time displays the next question
-                displayQuestions(questionIndex + 1);
-            }
-        });
+        // Displays list of choices
+        questionsSectionEl.appendChild(listEl);
+        
+        // Loop for listing off each choice
+        for (var i = 0; i < 4; i++) {
+            // Create list item element
+            var listItemEl = document.createElement("li");
+            // Sets ID
+            listItemEl.setAttribute("id","listItemEl");
+            // Selects choice, sets text content of list item element
+            listItemEl.textContent = questionsArray[questionIndex].choices[i];
+            // Adds list item to list of questions
+            listEl.appendChild(listItemEl);
+            // Make list items clickable and see if the item clicked is the right answer
+            listItemEl.addEventListener("click", function(event) {
+                var option = event.target;
+
+                if (option.textContent === questionsArray[questionIndex].answer) {
+                    // Runs the function all over again, except this time displays the next question
+                    displayQuestions(questionIndex + 1);
+                }
+                else {
+                    // Subtract time from the clock
+                    timer = timer - 15;
+                    // Runs the function all over again, except this time displays the next question
+                    displayQuestions(questionIndex + 1);
+                }
+            });
+        }
+    } else {
+        endGame();
     }
 }
 
@@ -95,9 +104,55 @@ function displayQuestions(questionIndex) {
 function endGame() {
     clearContent();
 
+    // Heading
     var endGameH1 = document.createElement("h1");
+    endGameH1.setAttribute("id", "endGameH1");
     endGameH1.textContent = "GAME OVER";
     questionsSectionEl.appendChild(endGameH1);
+
+    // Paragraph
+    var endGameP = document.createElement("p");
+    endGameP.setAttribute("id","endGameP");
+    questionsSectionEl.appendChild(endGameP);
+
+    var score = timer;
+    timeEl.remove();
+    if (timer >= 0) {
+        endGameP.textContent = "Your score is: " + score;
+    } else {
+        endGameP.textContent = "Out of time! Your score: 0";
+    }
+
+    // Label
+    var endGameLabel = document.createElement("label");
+    endGameLabel.setAttribute("id","endGameLabel");
+    endGameLabel.textContent = "Enter initials: ";
+    questionsSectionEl.appendChild(endGameLabel);
+
+    // Input
+    var endGameInput = document.createElement("input");
+    endGameInput.setAttribute("type","text");
+    endGameInput.setAttribute("id","endGameInput");
+    endGameInput.textContent = "";
+    questionsSectionEl.appendChild(endGameInput);
+
+    // Submit button
+    var endGameSubmit = document.createElement("button");
+    endGameSubmit.setAttribute("type","submit");
+    endGameSubmit.setAttribute("id","endGameSubmit");
+    endGameSubmit.textContent = "Submit";
+    questionsSectionEl.appendChild(endGameSubmit);
+
+    endGameSubmit.addEventListener("click", function() {
+        var initials = endGameInput.value;
+
+        if (initials === "") {
+            window.alert("Please enter a value!");
+        }
+        else {
+            // STORE FINAL SCORE
+        }
+    })
 }
 
 
